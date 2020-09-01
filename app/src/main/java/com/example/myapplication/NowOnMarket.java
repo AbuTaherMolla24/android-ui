03882726e296dc309fc3b6909f07fbde2815cf8c
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-09-01 16:19:08 +0600

    top market part update
    top market part update
    1.

---
+++ app/src/main/java/com/example/myapplication/NowOnMarket.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NowOnMarket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_on_market);
+    }
+}
